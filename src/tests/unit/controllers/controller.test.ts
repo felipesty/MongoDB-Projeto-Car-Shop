74422ab237843771149002528165925fddc10fbb
felipesty <felipestylex08@gmail.com>
2022-10-29 14:15:30 -0300

    testes de controller

---
+++ src/tests/unit/controllers/controller.test.ts
@@ -0,0 +1,50 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import CarController from '../../../controllers/CarController';
+import { carMock, carMockId } from '../../mocks/carMock';
+import { Response, Request, NextFunction } from 'express';
+const { expect } = chai;
+
+describe('Car Controller', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(async () => {
+    sinon.stub(carService, 'create').resolves(carMockId);
+    sinon.stub(carService, 'readOne').resolves(carMockId);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('Create Car', () => {
+    it('Success', async () => {
+      req.body = carMock;
+
+      await carController.create(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockId)).to.be.true;
+    });
+  })
+
+  describe('ReadOne Frame', () => {
+    it('Sucess', async () => {
+      req.params = { id: carMockId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockId)).to.be.true;
+    });
+  })
+
+});
